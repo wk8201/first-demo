1e7fc7674529d4f3aab895f3bfe35f6f290b6a0d
王康 <wk89206@163.com>
2025-03-24 19:05:15 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    今天将会是一个新的开始
+  </div>
+  <HelloWorlds msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorlds from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorlds
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,9 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    port: 3000, // 指定端口为3000
+    open: true  // 可选：自动打开浏览器
+  }
+};
