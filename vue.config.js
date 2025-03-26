74c1bac6347211693182d864d812b5051b870c8f
王康 <wk89206@163.com>
2025-03-26 22:11:09 +0800

    config

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/first-demo/'  // 替换为您的仓库名
-    : '/'
-}
 module.exports = defineConfig({
   transpileDependencies: true
 })
 module.exports = {
+  publicPath: '/first-demo/',  // 必须与仓库名完全一致
   devServer: {
     port: 3000, // 指定端口为3000
     open: true  // 可选：自动打开浏览器
